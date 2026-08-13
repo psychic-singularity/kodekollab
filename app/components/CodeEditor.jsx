c63278a361a5cc8cc310b826c241afd6a562f702
psychic-singularity <kumarsinghpradyumna@gmail.com>
2026-08-14 00:33:53 +0530

    initial sockets

--- app/components/CodeEditor.jsx
+++ app/components/CodeEditor.jsx
@@ -2,7 +2,28 @@
 
 import Editor from "@monaco-editor/react";
 
+import { useEffect } from "react";
+import { socket } from "../lib/socket";
+
+
+
 export default function CodeEditor({ file, onChange }) {
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("Connected:", socket.id);
+
+      socket.emit("join-room", "test-room");
+    });
+
+    socket.on("user-joined", (userId) => {
+      console.log("New user joined:", userId);
+    });
+
+    return () => {
+      socket.off("connect");
+      socket.off("user-joined");
+    };
+  }, []);
   return (
     <Editor
       height="100%"
---
+++ server/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("join-room", (roomId) => {
+    socket.join(roomId);
+
+    console.log(`${socket.id} joined room ${roomId}`);
+
+    socket.to(roomId).emit("user-joined", socket.id);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+server.listen(5000, () => {
+  console.log("Server running on http://localhost:5000");
+});
